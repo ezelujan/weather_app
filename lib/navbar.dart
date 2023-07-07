dd8676ebb676cbc742127bd67c55becf80dd0248
Ezequiel Luján Escobar <ojos1098@gmail.com>
2023-07-06 23:51:18 -0500

    feat: navbar

---
+++ lib/navbar.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  var currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double displayWidth = MediaQuery.of(context).size.width;
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: displayWidth * .05,
+        vertical: displayWidth * .07,
+      ),
+      height: displayWidth * .155,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.1),
+            blurRadius: 30,
+            offset: const Offset(0, 10),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: ListView.builder(
+        itemCount: 4,
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: displayWidth * .02),
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            setState(() {
+              currentIndex = index;
+              HapticFeedback.lightImpact();
+            });
+          },
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Stack(
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 850),
+                curve: Curves.fastLinearToSlowEaseIn,
+                width: index == currentIndex
+                    ? displayWidth * .32
+                    : displayWidth * .18,
+                alignment: Alignment.center,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 850),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  height: index == currentIndex ? displayWidth * .12 : 0,
+                  width: index == currentIndex ? displayWidth * .32 : 0,
+                  decoration: BoxDecoration(
+                    color: index == currentIndex
+                        ? Colors.blueAccent.withOpacity(.2)
+                        : Colors.transparent,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 850),
+                curve: Curves.fastLinearToSlowEaseIn,
+                width: index == currentIndex
+                    ? displayWidth * .31
+                    : displayWidth * .18,
+                alignment: Alignment.center,
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 850),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          width: index == currentIndex ? displayWidth * .13 : 0,
+                        ),
+                        AnimatedOpacity(
+                          opacity: index == currentIndex ? 1 : 0,
+                          duration: const Duration(milliseconds: 850),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          child: Text(
+                            index == currentIndex ? listOfStrings[index] : '',
+                            style: const TextStyle(
+                              color: Colors.blueAccent,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 850),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          width:
+                              index == currentIndex ? displayWidth * .03 : 20,
+                        ),
+                        Icon(
+                          listOfIcons[index],
+                          size: displayWidth * .076,
+                          color: index == currentIndex
+                              ? Colors.blueAccent
+                              : Colors.black26,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<String> listOfStrings = [
+    'Home',
+    'Favorite',
+    'Account',
+    'Settings',
+  ];
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.person_rounded,
+    Icons.settings_rounded,
+  ];
+}
